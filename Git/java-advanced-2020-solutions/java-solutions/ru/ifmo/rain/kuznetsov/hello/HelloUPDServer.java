8f93e3b7dbd99e76559237c7d78fcb9a9a5ca846
vladrus13 <vladrus13rus@yandex.ru>
2020-04-20 17:50:41 +0300

    Autogenerated commit

--- Git/java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/kuznetsov/hello/HelloUPDServer.java
+++ Git/java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/kuznetsov/hello/HelloUPDServer.java
@@ -2,14 +2,47 @@ package ru.ifmo.rain.kuznetsov.hello;
 
 import info.kgeorgiy.java.advanced.hello.HelloServer;
 
-public class HelloUPDServer implements HelloServer {
-    @Override
-    public void start(int i, int i1) {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class HelloUPDServer implements HelloServer {
+
+    private int packageSize;
+    private DatagramSocket socket;
+    private ExecutorService workers;
+
+    @Override
+    public void start(int port, int threadCount) {
+        try {
+            socket = new DatagramSocket(port);
+            packageSize = socket.getSendBufferSize();
+            workers = Executors.newFixedThreadPool(threadCount);
+            for (int i = 0; i < threadCount; i++) {
+                workers.submit(() -> {
+                    while (!socket.isClosed()) {
+                        DatagramPacket packet = new DatagramPacket(new byte[packageSize], packageSize);
+                        try {
+                            socket.receive(packet);
+                            packet.setData(("Hello, " + new String(packet.getData(), packet.getOffset(), packet.getLength(), StandardCharsets.UTF_8)).getBytes(StandardCharsets.UTF_8));
+                            socket.send(packet);
+                            System.out.println("New answer\n");
+                        } catch (IOException ignored) { }
+                    }
+                });
+            }
+        } catch (SocketException e) {
+            // bad soket
+        }
     }
 
     @Override
     public void close() {
-
+        socket.close();
+        workers.shutdown();
     }
 }
